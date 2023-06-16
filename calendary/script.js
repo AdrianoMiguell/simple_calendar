3bf8d3e9b4181b01b29f44d584f7c395473fa6c3
AdrianoMiguell <adrianosantos260804@gmail.com>
2023-06-15 22:10:41 -0300

    version_modern

--- calendary/script.js
+++ calendary/script.js
@@ -19,15 +19,7 @@ const months = [
   "Dec",
 ];
 
-const weeks = [
-  "S",
-  "M",
-  "T",
-  "W",
-  "T",
-  "F",
-  "S",
-];
+const weeks = ["S", "M", "T", "W", "T", "F", "S"];
 
 let constructorMonth = {
   m: [],
@@ -37,7 +29,58 @@ let constructorMonth = {
 
 // const numberOfDays = [28, 30, 31];
 const year = new Date().getFullYear();
-Title.innerHTML += year;
+let nameYear = String(year).split("");
+
+for (const i in nameYear) {
+  Title.innerHTML += `<span class='titleOfYear'>${nameYear[i]}</span>`;
+}
+
+function createBubbles() {
+  const areaTitle = document.querySelector(".area-title");
+
+  // let num = (Math.floor(Math.random() * 3) + 1) * 10;
+  let num = 40;
+
+  for (let i = 0; i < num; i++) {
+    let bubble = document.createElement("div");
+
+    let nX;
+    let nY = 0;
+
+    nX = i + 20.5 * i + 1;
+    nY =
+      -(Math.floor(Math.random() * 200) + 10 - i) -
+      i * (Math.floor(Math.random() * 3) + 1);
+    c = Math.floor(Math.random() * 5) + 1;
+
+    bubble.setAttribute("class", "bubbles");
+    bubble.style.translate = `${nX}px ${nY}px`;
+
+    switch (c) {
+      case 1:
+        bubble.style.backgroundColor = "navy";
+        break;
+      case 2:
+        bubble.style.backgroundColor = "#197c7c";
+        break;
+      case 3:
+        bubble.style.backgroundColor = "palevioletred";
+        break;
+      case 4:
+        bubble.style.backgroundColor = "darksalmon";
+        break;
+      case 5:
+        bubble.style.backgroundColor = "#ffff4e";
+        break;
+      default:
+        bubble.style.backgroundColor = "#41ffc6";
+        break;
+    }
+
+    console.log(bubble);
+    areaTitle.appendChild(bubble);
+  }
+}
 
 function saveMonths() {
   for (let i = 0; i < numberOfMonths; i++) {
@@ -80,20 +123,34 @@ function writeMonths() {
     let div = document.createElement("div");
     div.setAttribute("id", String(constructorMonth.m[i]));
     div.setAttribute("class", "month");
-    div.innerHTML += `<h1> ${constructorMonth.m[i]} </h1>`;
+    div.innerHTML += `<div class='headCalender'> <div class='monthOfName'> <h1> ${constructorMonth.m[i]} </h1> </div> <div class='namesOfDays'> </div> </div>`;
     div.innerHTML += `<div class='days'> </div>`;
 
     calendary.appendChild(div);
 
+    colorCalender(i);
     writeDays(i);
+  }
+}
+
+function colorCalender(m) {
+  let headCalender = document.querySelectorAll(`.headCalender`);
+
+  if(m == 0 || m % 2 == 0) {
+    headCalender[m].style.backgroundColor = "#6eabf0";
+  } else if(m % 3 == 0) {
+    headCalender[m].style.backgroundColor = "#7df06e";
+  } else {
+    headCalender[m].style.backgroundColor = "#f06e6e";
   }
 }
 
 function writeDays(m) {
   let divDays = document.querySelectorAll(`.days`);
+  let namesOfDays = document.querySelectorAll(`.namesOfDays`);
 
   for (let i = 0; i < 7; i++) {
-    divDays[m].innerHTML += `<span class='namesOfWeek'> ${weeks[i]} </span>`;
+    namesOfDays[m].innerHTML += `<span> ${weeks[i]} </span>`;
   }
 
   for (let d = 1; d <= constructorMonth.firstDay[m]; d++) {
@@ -108,6 +165,7 @@ function writeDays(m) {
 }
 
 function initialize() {
+  // createBubbles();
   saveMonths();
   writeMonths();
 }
